5d7e80f1e440a0d650f4409bd9bbb76aa93fdeb5
romantix74 <romantix75@gmail.com>
2017-07-03 16:00:45 +0300

    2017.06.03

--- spisok.py
+++ spisok.py
@@ -1,4 +1,13 @@
 	
-for i in range(76,122+1):
-	print('10.5.9.{}'.format(i))
+#for i in range(76,122+1):
+#	print('10.5.9.{}'.format(i))
+
+out = []	
 	
+with open('input.txt', 'r') as f:
+	out = f.readlines()
+	
+for i,str in enumerate(out):
+	s = str.split()
+	#str(i)
+	print('10.5.9.{0} {1}'.format(i+96, s[1]))
